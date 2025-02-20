284cc6fbbd3c15198cf47a48dfdf56552271291e
fornari03 <221017023@aluno.unb.br>
2025-02-20 16:49:13 -0300

    feat: adiciona rota de /drivers/team/:team

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,8 @@ const drivers = [
   { id: 2, name: "George Russell", team: "Mercedes" },
   { id: 3, name: "Max Verstappen", team: "Red Bull Racing" },
   { id: 4, name: "Lando Norris", team: "McLaren" },
+  { id: 5, name: "Charles Leclerc", team: "Ferrari" },
+  { id: 6, name: "Valtteri Bottas", team: "Mercedes" },
 ];
 
 server.get("/teams", async (request, response) => {
@@ -33,6 +35,10 @@ interface DriverParams {
   id: string;
 }
 
+interface TeamParams {
+  team: string;
+}
+
 server.get<{ Params: DriverParams }>(
   "/drivers/:id",
   async (request, response) => {
@@ -45,6 +51,21 @@ server.get<{ Params: DriverParams }>(
     } else {
       response.type("application/json").code(404);
       return { message: "Driver not found" };
+    }
+  }
+);
+
+server.get<{ Params: TeamParams }>(
+  "/drivers/team/:team",
+  async (request, response) => {
+    const team = request.params.team;
+    const teamDrivers = drivers.filter((driver) => driver.team === team);
+    if (teamDrivers.length > 0) {
+      response.type("application/json").code(200);
+      return { teamDrivers };
+    } else {
+      response.type("application/json").code(404);
+      return { message: "Team not found" };
     }
   }
 );
